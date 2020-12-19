988fa1f49439b8903640d616b5009ab9069c3c40
AlexHuynh23 <70555816+AlexHuynh23@users.noreply.github.com>
2020-12-18 20:56:41 -0800

    Basic Ball collision and paddle

---
+++ newGame.js
@@ -0,0 +1,169 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var ballStatus = 0;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var velocityX = 2;    //Change this number lower or higher to adjust difficulty
+var velocityY = -2;   //Change this number lower or higher to adjust difficulty
+var paddleHeight = 15;
+var paddleWidth = 100;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickRowCount = 4;//Change this number lower or higher to adjust difficulty
+var brickColumnCount = Math.floor(canvas.width/brickWidth) - 1;
+var brickPadding = 10;
+var brickOffsetVertical = 30;
+var brickOffsetHorizontal = 25;
+
+var bricks = [];
+for (var i = 0; i < brickColumnCount; i++) {
+    bricks[i] = [];
+    for (var j = 0; j < brickRowCount; j++) {
+        bricks[i][j] = { x: 0, y: 0, status: 1 };
+    }
+}
+
+document.addEventListener("keydown", keyPressed, false);
+document.addEventListener("keyup", keyUnpressed, false);
+
+function keyPressed(e) {
+  /*
+  */
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+    else if (e.keyCode == 32){
+      ballStatus = 1;
+      x = canvas.width / 2;
+      y = canvas.height - 30;
+    }
+}
+
+
+
+function keyUnpressed(e) {
+  /*
+  */
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+
+
+function collisionDetection() {
+  /*
+  */
+    for (var i = 0; i < brickColumnCount; i++) {
+        for (var j = 0; j < brickRowCount; j++) {
+            var b = bricks[i][j];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    velocityY = -velocityY;
+                    b.status = 0;
+                    ballStatus = 0;
+                }
+            }
+        }
+    }
+}
+
+
+
+function drawBall() {
+  /*
+  */
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#ffffff";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+
+function drawPaddle() {
+  /*
+  */
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#ffffff";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+function drawBricks() {
+  /*
+  */
+    for (var i = 0; i < brickColumnCount; i++) {
+        for (var j = 0; j < brickRowCount; j++) {
+            if (bricks[i][j].status == 1) {
+                var brickX = (i * (brickWidth + brickPadding)) + brickOffsetHorizontal;
+                var brickY = (j * (brickHeight + brickPadding)) + brickOffsetVertical;
+                bricks[i][j].x = brickX;
+                bricks[i][j].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#ffffff";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+
+function draw() {
+  /*
+  */
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
+
+    drawPaddle();
+    collisionDetection();
+    if (ballStaus = 1){
+      drawBall();
+    }
+
+    if (x + velocityX > canvas.width - ballRadius || x + velocityX < ballRadius) {
+        velocityX = -velocityX;
+    }
+    if (y + velocityY < ballRadius) {
+        velocityY = -velocityY;
+    }
+    else if (y + velocityY > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            if (y = y - paddleHeight) {
+                velocityY = -velocityY;
+            }
+        }
+        else {
+            alert("YOU LOST");
+            document.location.reload();
+            clearInterval(interval); // Needed for Chrome to end game
+        }
+    }
+
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    }
+    else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+    x += velocityX;
+    y += velocityY;
+}
+
+var interval = setInterval(draw, 10);
