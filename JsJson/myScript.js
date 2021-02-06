985d3c7b0bc66fab4c6b16aa6dfb91a326e6c289
AlexHuynh23 <70555816+AlexHuynh23@users.noreply.github.com>
2021-02-05 20:17:14 -0800

    Json Fixed

--- JsJson/myScript.js
+++ JsJson/myScript.js
@@ -5,8 +5,8 @@ console.log(myDiv);
 // Click event to attach to button
 function myClick () {
   // Get the values that were input into the two text boxes.
-  var username = document.getElementById('username');
-  var password = document.getElementById('password');
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
   var Gmail = document.getElementById("Gmail");
   var Yahoo = document.getElementById("Yahoo");
   var ICloud = document.getElementById("ICloud");
@@ -23,22 +23,22 @@ function myClick () {
 
   if (Gmail.checked) {
     myDiv.innerHTML += "\t\tGmail Login\n";
-    myJSON['email'] = "Gmail";
+    myObj['email'] = "Gmail";
   }
   else if (Yahoo.checked) {
     myDiv.innerHTML += "\t\tYahoo Login\n";
-    myJSON['email'] = "Yahoo";
+    myObj['email'] = "Yahoo";
   }
   else if (ICloud.checked) {
     myDiv.innerHTML += "\t\tICloud Login\n";
-    myJSON['email'] = "ICloud";
+    myObj['email'] = "ICloud";
   }
   else if (Bishops.checked) {
     myDiv.innerHTML += "\t\tBishops Login\n";
-    myJSON['email'] = "Bishops";
+    myObj['email'] = "Bishops";
   }
   else {
-    myJSON['email'] = "n/a";
+    myObj['email'] = "n/a";
   }
 
 
@@ -47,25 +47,41 @@ function myClick () {
 
 
   // Notice here that we are appending the values of the variables.
-  myDiv.innerHTML += "\t\t<p>" + myJSON['username'] + "'s email  is: " + myJSON['email'] + "</p>\n";
+  myDiv.innerHTML += "\t\t<p>" + myObj['username'] + "'s email  is: " + myObj['email'] + "</p>\n";
+  myDiv.innerHTML += "<button onclick='revert()'>Back</button>"
+}
+function revert () {
+  myDiv.innerHTML = "<label for='username'>Username:</label><input id='username' name='username' type='text' /><br />"
+  myDiv.innerHTML += "<label for='password'>Password:</label><input id='password' name='password' type='text' />"
+  myDiv.innerHTML += "<input type='radio' id='Gmail' name='pet' value='Gmail'><label for='Gmail'>Gmail</label><br/>"
+  myDiv.innerHTML += '<input type="radio" id="Yahoo" name="pet" value="Yahoo"><label for="Yahoo">Yahoo</label><br />'
+  myDiv.innerHTML += '<input type="radio" id="ICloud" name="pet" value="ICloud"><label for="ICloud">ICloud</label><br />'
+  myDiv.innerHTML += '<input type="radio" id="Bishops" name="pet" value="Bishops"><label for="Bishops">Bishops</label><br />'
+  myDiv.innerHTML += "<button onclick='myClick()'>Register Account</button>"
+  myDiv.innerHTML += "<button onclick='myClick1()'>Find Accounts</button>"
 }
 
 function myClick1 () {
-  myDiv.innerHTML = "\n<div id = 'search'><label for='username'>Search Username:</label><input id='usernameSearch' name='username' type='text' />";
+  myDiv.innerHTML = "\n<label for='username'>Search Username:</label><input id='usernameSearch' name='username' type='text' />";
   myDiv.innerHTML += "<button onclick='searchUser()'>Search By UserName</button><br/>";
   myDiv.innerHTML += "<input type='radio' id='Gmail' name='pet' value='Gmail'><label for='Gmail'>Gmail</label><br/>"
   myDiv.innerHTML += '<input type="radio" id="Yahoo" name="pet" value="Yahoo"><label for="Yahoo">Yahoo</label><br />'
   myDiv.innerHTML += '<input type="radio" id="ICloud" name="pet" value="ICloud"><label for="ICloud">ICloud</label><br />'
   myDiv.innerHTML += '<input type="radio" id="Bishops" name="pet" value="Bishops"><label for="Bishops">Bishops</label><br />'
-  myDiv.innerHTML += "<button onclick='searchEmail()'>Search By Email</button><br/><div/>";
+  myDiv.innerHTML += "<button onclick='searchEmail()'>Search By Email</button><br/>";
 
 }
 
 function searchUser(){
   text = localStorage.getItem("testJSON");
   obj = JSON.parse(text);
-  document.getElementById("demo").innerHTML = obj.name;
+  document.getElementById("myDiv").innerHTML = "\n<p>" + obj['username'] + "<p/>";
+  console.log(obj);
+
+
 }
+
+
 function searchEmail(){
 
 }
