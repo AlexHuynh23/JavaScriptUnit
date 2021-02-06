bdf93b9fbd1d0198986e85bf77d67a4af47e9f9d
AlexHuynh23 <70555816+AlexHuynh23@users.noreply.github.com>
2021-02-05 21:43:47 -0800

    to checkout

--- JsJson/myScript.js
+++ JsJson/myScript.js
@@ -12,44 +12,52 @@ function myClick () {
   var ICloud = document.getElementById("ICloud");
   var Bishops = document.getElementById("Bishops");
 
-  myObj = {
-    "username": username,
-    "password": password,
-    "email": '',
+  var len = 0;
+  text = localStorage.getItem("testJSON");
+  if (text != null){
+    myObj = JSON.parse(text);
+    len = myObj.length;
+    myObj[len] += [username, password, ''];
+
+  } else {
+    myObj = [[username, password, '']];
   }
+
+
 
   // By using = we *replace* the entire contents of the div tag.
   myDiv.innerHTML = "\n";
 
   if (Gmail.checked) {
     myDiv.innerHTML += "\t\tGmail Login\n";
-    myObj['email'] = "Gmail";
+    myObj[0][2] = "Gmail";
   }
   else if (Yahoo.checked) {
     myDiv.innerHTML += "\t\tYahoo Login\n";
-    myObj['email'] = "Yahoo";
+    myObj[0][2] = "Yahoo";
   }
   else if (ICloud.checked) {
     myDiv.innerHTML += "\t\tICloud Login\n";
-    myObj['email'] = "ICloud";
+    myObj[0][2] = "ICloud";
   }
   else if (Bishops.checked) {
     myDiv.innerHTML += "\t\tBishops Login\n";
-    myObj['email'] = "Bishops";
+    myObj[0][2] = "Bishops";
   }
   else {
-    myObj['email'] = "n/a";
+    myObj[0][2] = "n/a";
   }
 
 
   myJSON = JSON.stringify(myObj);
   localStorage.setItem("testJSON", myJSON);
-
-
+  stringify1(myJSON[0][1]);
+  console.log(myJSON[0][1]);
   // Notice here that we are appending the values of the variables.
-  myDiv.innerHTML += "\t\t<p>" + myObj['username'] + "'s email  is: " + myObj['email'] + "</p>\n";
+  myDiv.innerHTML += "\t\t<p>Account Created</p>\n";
   myDiv.innerHTML += "<button onclick='revert()'>Back</button>"
 }
+
 function revert () {
   myDiv.innerHTML = "<label for='username'>Username:</label><input id='username' name='username' type='text' /><br />"
   myDiv.innerHTML += "<label for='password'>Password:</label><input id='password' name='password' type='text' />"
@@ -64,24 +72,23 @@ function revert () {
 function myClick1 () {
   myDiv.innerHTML = "\n<label for='username'>Search Username:</label><input id='usernameSearch' name='username' type='text' />";
   myDiv.innerHTML += "<button onclick='searchUser()'>Search By UserName</button><br/>";
-  myDiv.innerHTML += "<input type='radio' id='Gmail' name='pet' value='Gmail'><label for='Gmail'>Gmail</label><br/>"
-  myDiv.innerHTML += '<input type="radio" id="Yahoo" name="pet" value="Yahoo"><label for="Yahoo">Yahoo</label><br />'
-  myDiv.innerHTML += '<input type="radio" id="ICloud" name="pet" value="ICloud"><label for="ICloud">ICloud</label><br />'
-  myDiv.innerHTML += '<input type="radio" id="Bishops" name="pet" value="Bishops"><label for="Bishops">Bishops</label><br />'
-  myDiv.innerHTML += "<button onclick='searchEmail()'>Search By Email</button><br/>";
-
 }
 
 function searchUser(){
+  var usernameSearch = document.getElementById('usernameSearch').value;
   text = localStorage.getItem("testJSON");
   obj = JSON.parse(text);
-  document.getElementById("myDiv").innerHTML = "\n<p>" + obj['username'] + "<p/>";
+  myDiv.innerHTML = "\n"
   console.log(obj);
+
+  // Displays all usernames that match the name
+  for (var i = 0, len = obj.length; i < len; i++){
+      if (obj[i]["username"] == usernameSearch.value)
+        myDiv.innerHTML += '<p>' + obj[i]["username"] + '<p>'
+  }
 
 
 }
-
-
-function searchEmail(){
-
+function stringify1 (x) {
+    console.log(Object.prototype.toString.call(x));
 }
